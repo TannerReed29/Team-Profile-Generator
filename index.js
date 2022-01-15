26337f1f73746c33fe4472eb8c18814049c93d9e
Tanner Brieger <tanner29@gmail.com>
2022-01-15 05:45:24 -0600

    added and setup classes, working on user prompt

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./src/');
+const generatePage = require('./src/page-template');
+
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Employee = require('./lib/Employee');
+const Intern = require('./lib/Intern');
+
+
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is the role of the Employee?',
+            choices: ['Manager', 'Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter the name of the Employee. '
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter the Id Number of the Employee. '
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter the Email of the Employee. '
+        }
+    ])
+
+    .then(({role, name, id, email}) => {
+
+    })
+}
+
+
+promptUser()
